5091a8786f50b6280bf4ab794cfb23a6434c2e39
jorge.carmona@vertisystem.com <jorge.carmona@vertisystem.com>
2023-08-31 13:06:19 -0400

    added quiz for switch-statment

---
+++ lesson-6/Quizzes/05-js-switch.js
@@ -0,0 +1,58 @@
+/**
+ * This quiz is to test your knowledge of Switch Statements.
+ *
+ * 1. Create a repository by cloning: https://github.com/jorgecarmona/npm-project-template
+ * 2. Follow the README for instructions
+ * 3. When you finish the quiz, create a PR on you repository
+ * 4. Submit to your lead
+ */
+
+/**
+ * Question 1
+ * Given a variable color with a value "red", use a switch statement to set a variable feeling to "passionate" for "red", "calm" for "blue", and "neutral" for other colors.
+ */
+
+/**
+ * Question 2
+ * Using a switch statement, check the value of the variable day (e.g., "Monday"). Return "Working day" for weekdays and "Weekend" for Saturday and Sunday.
+ */
+
+/**
+ * Question 3
+ * Given a variable score of 85, set a grade: "A" for 90 and above, "B" for 80-89, "C" for 70-79, "D" for 60-69, and "F" for below 60.
+ */
+
+/**
+ * Question 4
+ * Use a switch statement to determine if the variable fruit is a "berry". Set "berry" for strawberries, blueberries, and raspberries. "Not a berry" for other fruits.
+ */
+
+/**
+ * Question 5
+ * Check the value of the variable month (e.g., "January") and return which quarter of the year it belongs to.
+ */
+
+/**
+ * Question 6
+ * Use a switch statement to determine if a number is "small", "medium", or "large". Consider numbers 1-3 as "small", 4-6 as "medium", and 7-9 as "large".
+ */
+
+/**
+ * Question 7
+ * Determine the type of the variable pet (e.g., "dog"). Return "Canine" for a dog, "Feline" for a cat, and "Unknown" for other pets.
+ */
+
+/**
+ * Question 8
+ * Given a variable transportMode with a value like "car", return "Fast" for "plane", "Medium" for "car", and "Slow" for "bicycle".
+ */
+
+/**
+ * Question 9
+ * For a variable direction with values like "N", determine the full direction name. "N" for "North", "S" for "South", etc.
+ */
+
+/**
+ * Question 10
+ * Determine the type of drink based on the variable drink. "Water" for "H2O", "Coffee" for "C8H10N4O2", and "Unknown" for other values.
+ */
