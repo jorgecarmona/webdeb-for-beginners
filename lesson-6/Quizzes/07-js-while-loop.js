04f13f006f7f100cd6e5938440d462654b12ed89
jorge.carmona@vertisystem.com <jorge.carmona@vertisystem.com>
2023-09-06 13:42:38 -0400

    Added test for while loops

---
+++ lesson-6/Quizzes/07-js-while-loop.js
@@ -0,0 +1,58 @@
+/**
+ * This quiz is to test your knowledge of while-Loop.
+ *
+ * 1. Create a repository by cloning: https://github.com/jorgecarmona/npm-project-template
+ * 2. Follow the README for instructions
+ * 3. When you finish the quiz, create a PR on you repository
+ * 4. Submit to your lead
+ */
+
+/**
+ * Question 1
+ * Using a while loop, sum all numbers from 1 to 10 and store the result in a variable called sum.
+ */
+
+/**
+ * Question 2
+ * Using a while loop, create a string of numbers from 5 to 15 separated by commas, e.g., "5,6,7,...,15".
+ */
+
+/**
+ * Question 3
+ * Print all even numbers between 10 and 30 using a while loop.
+ */
+
+/**
+ * Question 4
+ * Determine the smallest power of 2 greater than 1000 using a while loop.
+ */
+
+/**
+ * Question 5
+ * Reverse a string "HelloWorld" using a while loop.
+ */
+
+/**
+ * Question 6
+ * Using a while loop, count how many times the letter 'a' appears in the string "banana".
+ */
+
+/**
+ * Question 7
+ * Using a while loop, compute the factorial of 5.
+ */
+
+/**
+ * Question 8
+ * Print all numbers from 100 to 80 in descending order using a while loop.
+ */
+
+/**
+ * Question 9
+ * Using a while loop, find the greatest common divisor (GCD) of two numbers, 56 and 98.
+ */
+
+/**
+ * Question 10
+ * Calculate the sum of all odd numbers between 1 and 50 using a while loop.
+ */
