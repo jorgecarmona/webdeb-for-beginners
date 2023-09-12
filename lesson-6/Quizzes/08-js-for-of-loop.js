0d3a168382dab0e9b04ba8417fbb610bf54a8f6e
jorge.carmona@vertisystem.com <jorge.carmona@vertisystem.com>
2023-09-11 21:10:25 -0400

    added quiz 08

---
+++ lesson-6/Quizzes/08-js-for-of-loop.js
@@ -0,0 +1,106 @@
+/**
+ * This quiz is to test your knowledge of for-of-Loop.
+ *
+ * 1. When you finish the quiz, create a PR on you repository
+ * 2. Submit to your lead
+ */
+
+// Question 1: Loop through the array and print the names of the Apostles
+const apostles = ["Peter", "James", "John", "Andrew", "Philip"];
+
+// Answer should be: Peter, James, John, Andrew, Philip (each in a new line)
+
+// Question 2: Calculate the sum of the ages of the Patriarchs
+const patriarchAges = [930, 912, 905, 895];
+
+// console.log(answer) // Answer should be: 3642
+
+// Question 3: Print the plagues of Egypt
+const plagues = ["Frogs", "Locusts", "Hail"];
+
+// Answer should be: Frogs, Locusts, Hail (each in a new line)
+
+// Question 4: Convert Bible books to uppercase
+const books = ["Genesis", "Exodus", "Leviticus"];
+
+// Answer should be: GENESIS, EXODUS, LEVITICUS (each in a new line)
+
+// Question 5: Count the number of times 'apple' appears in the array
+const fruitsInEden = ["apple", "pear", "apple", "grape"];
+
+// Answer should be: 2
+
+// Question 6: Multiply all elements in the array by 2
+const davidStones = [1, 2, 3];
+
+// Answer should be: [2, 4, 6]
+
+// Question 7: Reverse the string
+const nameOfGod = "Yahweh";
+
+// Answer should be: hewhaY
+
+// Question 8: Create a sentence by adding spaces to words
+const peaceBeUponYou = ["Peace", "be", "upon", "you"];
+
+// Answer should be: Peace be upon you
+
+// Question 9: Print out every other element from the array
+const daysOfCreation = ["Day1", "Day2", "Day3", "Day4", "Day5", "Day6"];
+
+// Answer should be: Day1, Day3, Day5 (each in a new line)
+
+// Question 10: Concatenate all the strings in the array
+const attributesOfGod = ["Omnipotent", "Omnipresent", "Omniscient"];
+
+// Answer should be: OmnipotentOmnipresentOmniscient
+
+// Question 11: Create a new array with only the numbers greater than 3
+const biblicalNumbers = [1, 3, 7, 12];
+
+// Answer should be: [7, 12]
+
+// Question 12: Calculate the product of all the elements in the array
+const disciplesAges = [30, 25, 40, 50];
+
+// Answer should be: 1500000
+
+// Question 13: Replace 'Goliath' with 'David' in the array
+const combatants = ["Goliath", "Soldier1", "Soldier2"];
+
+// Answer should be: ["David", "Soldier1", "Soldier2"]
+
+// Question 14: Print the square of each element in the array
+const squaresOfNumbers = [1, 2, 3, 4];
+
+// Answer should be: 1, 4, 9, 16 (each in a new line)
+
+// Question 15: Count the number of vowels in the string
+const theWord = "Bible";
+
+// Answer should be: 2
+
+// Question 16: Print the elements that are divisible by 5
+const numbersFromBible = [5, 10, 15, 20, 25];
+
+// Answer should be: 5, 10, 15, 20, 25 (each in a new line)
+
+// Question 17: Create a new string where the first letter of each word is capitalized
+const phrase = "in the beginning";
+
+// Answer should be: In The Beginning
+
+// Question 18: Create a new array where each element is double the original
+const loavesAndFishes = [2, 5];
+
+// Answer should be: [4, 10]
+
+// Question 19: Count the number of elements that are equal to 'manna'
+const foodInDesert = ["manna", "quail", "manna", "manna"];
+
+// Answer should be: 3
+
+// Question 20: Create a new array by picking every 3rd element from the original array
+const theCommandments = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// Answer should be: [3, 6, 9]
